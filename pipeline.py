3acca0a7a9079c31dc272ced05f670241df8af7c
nlccesc <93630735+nlccesc@users.noreply.github.com>
2024-04-29 22:11:57 +0800

    Update pipeline.py
    added contract and error handling

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,15 @@
 from web3 import Web3
 
 def blockchain_connection():
-    w3 = Web3(Web3.HTTPProvider('http://mainnet.infura.io/v3/project_id'))
-    if w3.is_connected:
-        print("Connected to the ETH blockchain.")
-    else:
-        print("Failed to connect to the ETH blockchain.")
-    return w3
+    try:
+        w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/e716b1fec5d34560a1e3f8906b3d9642'))
+        if w3.is_connected:
+            print("Connected to the ETH blockchain.")
+        else:
+            print("Failed to connect to the ETH blockchain.")
+        return w3
+    except Exception as e:
+        print(f"An error has occured: {str(e)}")
+        return None
+
+blockchain_connection()
