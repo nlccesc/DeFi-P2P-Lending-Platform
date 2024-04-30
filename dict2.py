3b4c50e211f0e633b76c843c846d00b9721cd53a
nlccesc <93630735+nlccesc@users.noreply.github.com>
2024-04-30 14:41:22 +0800

    Add files via upload
    dictionary for some definitions

---
+++ dict2.py
@@ -0,0 +1,17 @@
+abi_paths = {
+    'disbursement': 'C:\\Users\\haoha\\OneDrive\\Desktop\\personal\\Projects\\Hackathons\\hacksingapore 2024\\DeFi P2P Lending\\abi\\disbursement.json',
+    'interest calculation': 'C:\\Users\\haoha\\OneDrive\\Desktop\\personal\\Projects\\Hackathons\\hacksingapore 2024\\DeFi P2P Lending\\abi\\interest calculation.json',
+    'loan agreement': 'C:\\Users\\haoha\\OneDrive\\Desktop\\personal\\Projects\\Hackathons\\hacksingapore 2024\\DeFi P2P Lending\\abi\\loan agreement.json',
+    'repayment': 'C:\\Users\\haoha\\OneDrive\\Desktop\\personal\\Projects\\Hackathons\\hacksingapore 2024\\DeFi P2P Lending\\abi\\repayment.json',
+    'smart contract': 'C:\\Users\\haoha\\OneDrive\\Desktop\\personal\\Projects\\Hackathons\\hacksingapore 2024\\DeFi P2P Lending\\abi\\smart_contract.json',
+}
+
+contract_addresses = {
+    'disbursement': '0xf8e81D47203A594245E36C48e151709F0C19fBe8',
+    'interest calculation': '0xd2a5bC10698FD955D1Fe6cb468a17809A08fd005',
+    'loan agreement': '0x0fC5025C764cE34df352757e82f7B5c4Df39A836',
+    'repayment': '0x9d83e140330758a8fFD07F8Bd73e86ebcA8a5692',
+    'smart contract': '0xd9145CCE52D386f254917e481eB44e9943F39138'
+}
+
+private_key = "iEtLPC1tdEV6PfX53l/qQftvzuoUqG8sjAZUFWRhjOc18FRNc/x7Iw"
