73e4d49fdb2c4eacc571f567947bdefdebd0b7f2
nlccesc <93630735+nlccesc@users.noreply.github.com>
2024-05-02 10:54:23 +0800

    Add files via upload
    implement aes and e2ee encryption for non clunky ux

---
+++ security/encryption.py
@@ -0,0 +1,20 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import base64
+
+def generate_aes_key():
+    return get_random_bytes(32)
+
+def aes_encrypt(data, key):
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data.encode())
+    return base64.b64encode(cipher.nonce + tag + ciphertext).decode()
+
+def aes_decrypt(encrypted_data, key):
+    decoded_data = base64.b64decode(encrypted_data)
+    nonce = decoded_data[:16]
+    tag = decoded_data[16:32]
+    ciphertext = decoded_data[32:]
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    decrypted_data = cipher.decrypt_and_verify(ciphertext, tag)
+    return decrypted_data.decode()
