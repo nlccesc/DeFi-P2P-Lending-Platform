49436ad91e261e4f2524a25be36338175976e1ce
nlccesc <93630735+nlccesc@users.noreply.github.com>
2024-05-03 20:54:16 +0800

    Add files via upload
    put into isolated files for modularity

---
+++ security/crypto/encryption.py
@@ -0,0 +1,15 @@
+from Crypto.Cipher import AES
+from manage_key import generate_aes_key
+import base64
+import logging
+
+def aes_encrypt(data, key=None):
+    
+    if key is None:
+        key = generate_aes_key()
+    cipher = AES.new(key, AES.MODE_GCM)
+    ciphertext, tag = cipher.encrypt_and_digest(data.encode())
+    encrypted_data = base64.b64encode(cipher.nonce + tag + ciphertext).decode()
+    logging.info("Data encrypted")
+    return encrypted_data, key
+
